ff6756d0a40feb3f56c3157fa9191b108418e221
mpyw <ryosuke_i_628@yahoo.co.jp>
2021-06-29 20:10:38 +0900

    test: 💍 Add detailed tests

--- tests/FixedPasswordCryptorTest.php
+++ tests/FixedPasswordCryptorTest.php
@@ -42,7 +42,7 @@ class FixedPasswordCryptorTest extends TestCase
         $this->assertNotFalse($anotherCryptor->decrypt($encryptedA));
     }
 
-    public function testInvalidIv(): void
+    public function testInvalidIvLength(): void
     {
         $cryptor = new FixedPasswordCryptor('password');
         $this->assertFalse($cryptor->decrypt(''));
@@ -53,7 +53,44 @@ class FixedPasswordCryptorTest extends TestCase
         } catch (DecryptionFailedException $e) {
             $this->assertSame('', $e->getData());
             $this->assertSame('Failed to decrypt.', $e->getMessage());
-            $this->assertSame('error:06065064:digital envelope routines:EVP_DecryptFinal_ex:bad decrypt', $e->getOriginalMessage());
+            $this->assertSame('invalid iv length.', $e->getOriginalMessage());
+        }
+    }
+
+    public function testInvalidTagLength(): void
+    {
+        $cryptor = new FixedPasswordCryptor('password', new Cryptor('aes-256-gcm'));
+        $this->assertFalse($cryptor->decrypt(str_repeat('x', 16)));
+
+        try {
+            $cryptor->mustDecrypt(str_repeat('x', 16));
+            $this->assertTrue(false);
+        } catch (DecryptionFailedException $e) {
+            $this->assertSame(str_repeat('x', 16), $e->getData());
+            $this->assertSame('Failed to decrypt.', $e->getMessage());
+            $this->assertSame('invalid tag length.', $e->getOriginalMessage());
+        }
+    }
+
+    public function testInvalidTagContent(): void
+    {
+        $cryptor = new FixedPasswordCryptor('password', new Cryptor('aes-256-gcm'));
+
+        $corrupted = substr_replace(
+            $cryptor->encrypt(''),
+            str_repeat('x', 16),
+            -16
+        );
+
+        $this->assertFalse($cryptor->decrypt($corrupted));
+
+        try {
+            $cryptor->mustDecrypt($corrupted);
+            $this->assertTrue(false);
+        } catch (DecryptionFailedException $e) {
+            $this->assertSame($corrupted, $e->getData());
+            $this->assertSame('Failed to decrypt.', $e->getMessage());
+            $this->assertSame('invalid tag content.', $e->getOriginalMessage());
         }
     }
 }
